e8f2cdf6515fa4216569bf23602b9af5119e4452
Ken Power <github@kenpower.com>
2016-09-30 17:53:37 +0100

    comments

--- a.cpp
+++ a.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 using namespace std;
  
+<<<<<<< HEAD
+//This is a comment (A) 
+=======
+//this is another comment (B)
+//this comment should not be merged!
+>>>>>>>
+
 int main ()
 {
-   // for loop execution
-   for( int a = 10; a < 20; a = a + 1 )
+  
+   for( int i=0;i<10;i++ )
    {
-       cout << "value of a: " << a << endl;
+       cout << "have a terrible day day" << endl;
    }
- 
+   
+   cout << "C++ is the worst"
+   
    return 0;
 }
